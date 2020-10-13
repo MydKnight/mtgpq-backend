ce69a62922816c0e8c5b49005315590081bcc4d3
Shiloh <shiloh.madsen@gmail.com>
2020-10-13 11:52:40 -0700

    Moved creds to .env, created card model

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 import express from 'express';
-import routes from './src/routes/cardRoutes'
+import routes from './src/routes/cardRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = 8000;
 
+// Environment Variables
+dotenv.config({ silent: true })
+
+// Connect mongo to API
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/mtgpq?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(error => console.log(`Problem Connecting: ${error}`));
+
+// console.log(mongoose);
+  
+// Setup Body Parser - So incoming requests will be parsed through Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
 routes(app); 
 
-app.get('/', (req, res) => 
+app.get('/', (req, res, next) => 
     res.send(`Node and express server is up and running. Port: ${PORT}`)
 );
 
---
+++ src/models/cardModel.js
@@ -0,0 +1,95 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const CardSchema = new Schema({
+    id: {
+        type: Number
+    },
+    name: {
+        type: String,
+        required: 'Enter a card name'
+    },
+    imgurl: {
+        type: String
+    },
+    category: [{
+        name: String,
+        id: Number,
+        imgurl: String
+    }],
+    type: {type: String},
+    rarity: {
+        type: String
+    },
+    details: {
+        type: String
+    },
+    cost:{
+        type: Number
+    },
+    exclusive: {
+        type: Boolean
+    },
+    lootable: {
+        type: Boolean
+    },
+    compactName: {
+        type: String
+    },
+    color1:{
+        type: String
+    },
+    color2:{
+        type: String
+    },
+    color3:{
+        type: String
+    },
+    color4:{
+        type: String
+    },
+    color5:{
+        type: String
+    },
+    power:{
+        type: Number
+    },
+    toughness: {
+        type: Number
+    }, 
+    shield: {
+        type: Number
+    }, 
+    abilities: [{
+        id: {
+            type: Number
+        },
+        name: {
+            type: String
+        },
+        desc: {
+            type: String
+        }
+    }], 
+    subtypes: [{
+        id: {
+            type: Number
+        },
+        name: {
+            type: String
+        }
+    }], 
+    link:{
+        type: String
+    }, 
+    nightMatched: {
+        type: Boolean
+    },
+    nightSide: {
+        type: Boolean
+    }, 
+    owned: {
+        type: Boolean
+    }
+});
